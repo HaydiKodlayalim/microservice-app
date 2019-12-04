01f30c970001059252e77194392f91f1ab3214f2
tanertemel <taner.temel@coyoapp.com>
2019-12-04 23:57:55 +0100

    Eureka ve AccountServer baglantisi

--- account-service/src/main/java/com/haydikodlayalim/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/haydikodlayalim/accountservice/AccountServiceApplication.java
@@ -2,12 +2,14 @@ package com.haydikodlayalim.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
 
 }
